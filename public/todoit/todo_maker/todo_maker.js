f83cc52d70dc326c5638ad5e820798466ccbbe74
jeremyckahn <jeremyckahn@.(none)>
2010-11-14 17:45:36 -0800

    Added focus and blur functions/styles for todo_maker.

--- public/todoit/todo_maker/todo_maker.js
+++ public/todoit/todo_maker/todo_maker.js
@@ -1,4 +1,5 @@
-steal.plugins('jquery/controller',
+steal.css('todo_maker')
+	.plugins('jquery/controller',
 	'jquery/dom/form_params',
 	'jquery/view/ejs').then(function($){
 	$.Controller.extend('Todoit.todoMaker', 
@@ -13,6 +14,7 @@ steal.plugins('jquery/controller',
 		{
 			init: function(){
 				this.element.html(this.view(this.options));
+				this.find('input[type=text]').addClass('inactive');
 			},
 			
 			'form submit': function(el, ev){
@@ -25,7 +27,28 @@ steal.plugins('jquery/controller',
 					steal.dev.log('Making a todo: "' + todo + '"');
 					el.trigger("todo", todo);
 				}
-			} 
+			},
+			
+			'form input[type=text] focus': function(el, ev){
+				this.toggleFocus(el, true);
+			},
+			
+			'form input[type=text] blur': function(el, ev){
+				this.toggleFocus(el, false);
+			},
+			
+			toggleFocus: function(el, focused){
+				el.removeClass();
+				
+				el.toggleClass(function(){
+					if (focused){
+						return 'active';
+					} else {
+						return 'inactive';
+					}
+				})
+				
+			}
 		}	
 	)
 });
